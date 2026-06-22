832795acdef769dcf0164930b9019a73f060abbc
Fillipe <fillipeferreiranm@hotmail.com>
2026-06-22 19:51:20 -0300

    treinando po

--- src/exercicioPo/produto.java
+++ src/exercicioPo/produto.java
@@ -1,17 +1,30 @@
 package exercicioPo;
 
 public class produto {
-     String nome;
+    String nome;
     double preco;
     double desconto;
+    double pocetagem; // Mantive o nome da sua variável!
 
+    // 1º Método: Apenas faz a conta e retorna o valor
+    double precoComDesconto() {
+        return preco - (preco * (desconto / 100));
+    }
 
-    public void impressora (){
+    // 2º Método: Separado, serve apenas para imprimir os dados na tela
+    public void impressao_() {
+        System.out.println("Preço final: " + this.precoComDesconto());
+    }
 
-        System.out.print(this.preco);
-        System.out.printf(this.nome);
-        System.out.print(this.preco);
+    // 3º Método: Outra opção de impressão que você estava montando
+    public void main_impressao() {
+        System.out.println(this.nome);
+        System.out.println(this.preco);
+        System.out.println(this.desconto);
+        System.out.println(this.precoComDesconto()); // Imprime o cálculo direto
+
 
     }
 
-}
+
+    }
--- src/exercicioPo/produtoTest.java
+++ src/exercicioPo/produtoTest.java
@@ -9,25 +9,15 @@ public class produtoTest {
         produto caixa = new produto();
 
         caixa.nome = "caixa";
-        caixa.preco = 2312f;
-        caixa.desconto = 0.21;
+        caixa.preco = 100;
+        caixa.desconto = 10;
 
         System.out.println(caixa.nome);
         System.out.println(caixa.preco);
         System.out.println(caixa.desconto);
 
-        produto caixa0 = new produto();
+        caixa.precoComDesconto();
+        caixa.impressao_();
 
-        caixa0.nome = "caixa0";
-        caixa0.preco = 222f;
-        caixa0.desconto = 0.21;
-        System.out.println("------------------------------");
-        System.out.println(caixa0.nome);
-        System.out.println(caixa0.preco);
-        System.out.println(caixa0.desconto);
-        System.out.println("------------------------");
-        caixa0.impressora();
-        System.out.println("------------------------");
-        caixa.impressora();
     }
 }
